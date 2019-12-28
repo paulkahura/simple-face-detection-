9477627ecf917c32b3c07dda96bbb3ff1775a4e7
k0imburi <35740939+paulkahura@users.noreply.github.com>
2019-12-28 09:21:27 +0300

    Add files via upload
    work in progress

---
+++ image.py
@@ -0,0 +1,17 @@
+import cv2
+
+
+img=cv2.imread("img/galaxy.jpg",0)
+
+
+print(type(img))
+print(img)
+print(img.shape)
+print(img.ndim)
+
+resized_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
+print(resized_image.shape)
+cv2.imshow("galaxy",resized_image)
+cv2.imwrite("Galaxy_resized.jpg",resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
